f8d6edf67bb75a57719b6404e86dc29682d309c9
ZaiN <waqasrock141@gmail.com>
2023-11-14 00:48:28 +0530

    Running server for the 1st time
    First thing we did was to return a simple welcome note on the webpage using return HttpResponse.

--- Bookstore/app/views.py
+++ Bookstore/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def func(request):
+    return HttpResponse("<h2 style='text-align:center'>Welcome to my Django Project</h2>")
