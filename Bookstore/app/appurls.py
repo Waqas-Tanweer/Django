338edb45f7b8f0021861ca8fc05cc21a5646f7da
ZaiN <waqasrock141@gmail.com>
2023-11-15 21:34:45 +0530

    template inheritence
    Created a html file named  'base.html' and copied all the repeated codes to other html pages so that we prevent our every html page from having many no. of lines of codes
    Also made a dictionary in views page to fetch the data on our web page

--- Bookstore/app/appurls.py
+++ Bookstore/app/appurls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('waqas', views.func)]
+urlpatterns = [
+    path('index', views.func),
+]
--- Bookstore/app/views.py
+++ Bookstore/app/views.py
@@ -4,5 +4,16 @@ from django.urls  import reverse
 
 # Create your views here.
 
+# def func(request):
+#     return HttpResponse('Welcome to Django')
+
+d = {
+    'Harry Potter': ['Adventure', '1200'],
+    'Fifty Shades': ['Fantasy', '500'],
+    'Wings of Fire': ['Motivation', '1800'],
+}
+
+
+
 def func(request):
-    return render(request, "htmls\home.html")
+    return render(request, "htmls\home.html", {'data':d})
