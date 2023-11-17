003e65d8a89fcd391e830f7a4c35ff85b9668c12
ZaiN <waqasrock141@gmail.com>
2023-11-17 22:42:34 +0530

    Defining functions to fetch data
    Created 2 functions 1 that redirects us to the html page named index.html which shows the list of books available.
    The second function is triggered when we click on any book name on the index page and redirects us to another html page named 'details.html' which will display the details of that particular book that we clicked.

--- Bookstore/app/appurls.py
+++ Bookstore/app/appurls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'log'
+
 urlpatterns = [
     path('index', views.func),
+    path('<str:day>', views.func2, name = 'book-details')
 ]
--- Bookstore/app/views.py
+++ Bookstore/app/views.py
@@ -7,13 +7,18 @@ from django.urls  import reverse
 # def func(request):
 #     return HttpResponse('Welcome to Django')
 
-d = {
-    'Harry Potter': ['Adventure', '1200'],
-    'Fifty Shades': ['Fantasy', '500'],
-    'Wings of Fire': ['Motivation', '1800'],
+d = {'HP':{'cat' : 'adv'},
+     'FS':{'cat' : 'sex'},
+     'WOF':{'cat' : 'moti'},
 }
 
 
 
 def func(request):
     return render(request, "htmls\home.html", {'data':d})
+
+def func2(request, day):
+    day = day
+    c = list(d)
+    print(d)
+    return render(request, "htmls\details.html", {'data':d, 'c':c, 'day':day})
